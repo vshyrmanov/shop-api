33d2155b4432cbf54edfc4a8c63cba30a7dbbf97
vshyrmanov <shyrmanovvm@gmail.com>
2023-12-07 22:12:13 +0200

    test excel

--- index.js
+++ index.js
@@ -8,7 +8,62 @@ const ShoppingRouter = require('./Routes/Shopping.routes');
 const Users_shopRouter = require('./Routes/Users_shop.routes');
 const home = require('./home');
 
+const multer = require('multer');
+const excelToJson = require('convert-excel-to-json');
+const fs = require('fs-extra');
+
 const PORT = process.env.PORT || 80
+
+const upload = multer({dest: "uploads/"});
+
+app.post('/read', upload.single('file'), (req, res) => {
+  try {
+    if (req.file?.filename == null || req.file?.filename == "undefined") {
+      res.status(400).json('No file')
+    } else {
+      const filePath = 'uploads/' + req.file.filename
+
+      const excelData = excelToJson({
+        sourceFile: filePath,
+        
+        sheets: [{
+          name: "testik",
+          header: {
+            rows: 1
+          },
+          columnToKey: {
+            "A": "lastName",
+            "B": "Name",
+            "C": "fatherName",
+            "D": "speciality",
+            "E": "adress",
+          }
+        },
+        {
+          name: "TO",
+          header: {
+            rows: 1
+          },
+          columnToKey: {
+            "A": "lastName",
+            "B": "Name",
+            "C": "fatherName",
+            "D": "speciality",
+            "E": "adress"
+          }
+        }
+      ]
+        
+      })
+
+      fs.remove(filePath)
+
+      res.status(200).json(excelData)
+    }
+  } catch {
+    res.status(500)
+  }
+})
 
 app.use(express.json())
 app.use(cors())
